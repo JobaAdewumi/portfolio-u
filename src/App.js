de0646ab1e13859dc6552b008c4380a9de78e111
Joba Adewumi <jobaadewumis@gmail.com>
2022-04-27 09:51:59 +0100

    Removed the routes for the empty pages

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Home from './containers/Home';
-import Projects from './containers/Projects';
-import Contact from './containers/Contact';
-import CodingRepublic from './containers/CodingRepublic';
-import NotFound from './components/NotFound';
+// import Projects from './containers/Projects';
+// import Contact from './containers/Contact';
+// import CodingRepublic from './containers/CodingRepublic';
+// import NotFound from './components/NotFound';
 import Layout from './hocs/Layout';
 
 import store from './store';
@@ -19,10 +19,10 @@ const App = () => {
         <Layout>
           <Routes>
             <Route exact path='/' element={<Home />} />
-            <Route exact path='/projects' element={<Projects />} />
-            <Route exact path='/contact' element={<Contact />} />
-            <Route exact path='/codingrepublic' element={<CodingRepublic />} />
-            <Route exact path='*' element={<NotFound />} />
+            {/* <Route exact path='/projects' element={<Projects />} /> */}
+            {/* <Route exact path='/contact' element={<Contact />} /> */}
+            {/* <Route exact path='/codingrepublic' element={<CodingRepublic />} /> */}
+            {/* <Route exact path='*' element={<NotFound />} /> */}
           </Routes>
         </Layout>
       </Router>
