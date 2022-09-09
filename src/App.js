c2a66b453174a9add69ba49c2645b8af36e7a7f3
Joba Adewumi <jobaadewumis@gmail.com>
2022-09-09 22:10:11 +0100

    Removed redux and Provider

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import Home from './containers/Home';
-// import Projects from './containers/Projects';
-// import Contact from './containers/Contact';
-// import CodingRepublic from './containers/CodingRepublic';
-// import NotFound from './components/NotFound';
+import Podcast from './containers/Podcast';
+import Projects from './containers/Projects';
+import Contact from './containers/Contact';
+import CodingRepublic from './containers/CodingRepublic';
+import NotFound from './components/NotFound';
 import Layout from './hocs/Layout';
-
-import store from './store';
 
 import './sass/main.scss';
 
 const App = () => {
-  return (
-    <Provider store={store}>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            {/* <Route exact path='/projects' element={<Projects />} /> */}
-            {/* <Route exact path='/contact' element={<Contact />} /> */}
-            {/* <Route exact path='/codingrepublic' element={<CodingRepublic />} /> */}
-            {/* <Route exact path='*' element={<NotFound />} /> */}
-          </Routes>
-        </Layout>
-      </Router>
-    </Provider>
-  );
-}
+	return (
+		<Router>
+			<Layout>
+				<Routes>
+					<Route exact path='/' element={<Home />} />
+					<Route exact path='/podcast' element={<Podcast />} />
+					<Route exact path='/projects' element={<Projects />} />
+					<Route exact path='/contact' element={<Contact />} />
+					<Route exact path='/codingrepublic' element={<CodingRepublic />} />
+					<Route exact path='*' element={<NotFound />} />
+				</Routes>
+			</Layout>
+		</Router>
+	);
+};
 
 export default App;
