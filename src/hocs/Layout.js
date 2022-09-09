1661f5cdb1e20372aa591d80ffd2d9c1a70545e6
Joba Adewumi <jobaadewumis@gmail.com>
2022-09-09 22:12:11 +0100

    Removed classname to avoid errors

--- src/hocs/Layout.js
+++ src/hocs/Layout.js
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 const layout = (props) => (
-	<div className="layout">
+	<div>
 		<Navbar />
 		{props.children}
 		<Footer />
