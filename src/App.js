4fc55885699e913a5beca61968be951fad9b94c2
Joba Adewumi <jobaadewumis@gmail.com>
2022-10-17 20:09:40 +0100

    Commented unused route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NotFound from './components/NotFound';
+import Contact from './containers/Contact';
 import Home from './containers/Home';
 import Podcast from './containers/Podcast';
 import Projects from './containers/Projects';
-import Contact from './containers/Contact';
-import CodingRepublic from './containers/CodingRepublic';
-import NotFound from './components/NotFound';
 import Layout from './hocs/Layout';
 
 import './sass/main.scss';
@@ -19,7 +18,7 @@ const App = () => {
 					<Route exact path='/podcast' element={<Podcast />} />
 					<Route exact path='/projects' element={<Projects />} />
 					<Route exact path='/contact' element={<Contact />} />
-					<Route exact path='/codingrepublic' element={<CodingRepublic />} />
+					{/* <Route exact path='/codingrepublic' element={<CodingRepublic />} /> */}
 					<Route exact path='*' element={<NotFound />} />
 				</Routes>
 			</Layout>
