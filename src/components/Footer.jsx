454fbdd586006f9438d037c347307c5e0ccaeb31
Joba Adewumi <jobaadewumis@gmail.com>
2022-10-17 19:37:08 +0100

    Removed whatsapp and added proper links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,31 +4,49 @@ import { Link } from 'react-router-dom';
 const footer = () => {
 	return (
 		<footer className='footer'>
-            <Link to='/' className='footer__logo'>JOBA ADEWUMI</Link>
+			<Link to='/' className='footer__logo'>
+				JOBA ADEWUMI
+			</Link>
 			<ul className='footer__list'>
 				<li className='footer__list__item'>
-					<a href='https://www.linkedin.com/in/joba-adewumi' className='footer__list__item__linkedin'>
+					<a
+						href='https://linkedin.com/in/joba-adewumi'
+						target='_blank'
+						rel='noreferrer'
+						className='footer__list__item__linkedin'>
 						Linkedin
 					</a>
 				</li>
 				<li className='footer__list__item'>
-					<a href='https://twitter.com/_Jobaa' className='footer__list__item__twitter'>
+					<a
+						href='https://twitter.com/_Jobaa'
+						target='_blank'
+						rel='noreferrer'
+						className='footer__list__item__twitter'>
 						Twitter
 					</a>
 				</li>
 				<li className='footer__list__item'>
-					<a href='https://www.github.com/JobaAdewumi' className='footer__list__item__github'>
+					<a
+						href='https://github.com/JobaAdewumi'
+						target='_blank'
+						rel='noreferrer'
+						className='footer__list__item__github'>
 						Github
 					</a>
 				</li>
-				{/* TODO: Remmember to put the whatsapp link */}
+
 				<li className='footer__list__item'>
-					<a href='#' className='footer__list__item__whatsapp'>
-						WhatsApp
+					<a
+						href='https://linktr.ee/jobaadewumi'
+						target='_blank'
+						rel='noreferrer'
+						className='footer__list__item__whatsapp'>
+						Linktree
 					</a>
 				</li>
-            </ul>
-            <p className='footer__ending'>© 2022 by Joba Adewumi</p>
+			</ul>
+			<p className='footer__ending'>© 2022 by Joba Adewumi</p>
 		</footer>
 	);
 };
